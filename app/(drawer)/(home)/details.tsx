405d0bbac5509db215c38b219cab032ffc5c1f6b
niyoh55 <niyohedwyn21@gmail.com>
2025-03-19 19:02:46 +0800

    added go back button

--- app/(drawer)/(home)/details.tsx
+++ app/(drawer)/(home)/details.tsx
@@ -1,10 +1,20 @@
+import { useRouter } from 'expo-router';
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
 
 const DetailsScreen = () => {
+  const router = useRouter();
+
+  const goBack = () => {
+    // Navigate to the details screen
+    router.back();
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Details Screen</Text>
+      <Button onPress={goBack}>navigateToDetails</Button>
     </View>
   );
 };
